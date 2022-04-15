0f4eca1b17a2ca0e1e2ef59a8b1dbc7de2312874
Jer-Shen Chen <jschen1217@gmail.com>
2022-04-15 04:42:38 -0400

    update skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ProgressBar from "../features/PBar";
-//import * as ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom";
 
 class Skills extends Component {
   render() {
@@ -10,25 +10,23 @@ class Skills extends Component {
         return (
           <li className="list-inline-item mx-3" key={i}>
             <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
+              <div className="potion-body">
+                <div className={skills.level}></div>
               </div>
+              <br />
+              <i className={skills.class} style={{ fontSize: "220%" }}>
+                <p
+                  className="text-center"
+                  style={{ fontSize: "30%", marginTop: "4px" }}
+                >
+                  {skills.name}
+                </p>
+              </i>
             </span>
           </li>
         );
       });
     }
-    var ReactDOM = require("react-dom/client");
-
-    var pbar = <ProgressBar />;
-    var pbar = ReactDOM.creatRoot(document.getElementById("pbar"));
 
     return (
       <section
@@ -41,13 +39,80 @@ class Skills extends Component {
               <span className="text-white">{sectionName}</span>
             </h1>
           </div>
+          {/* <div
+            style={{
+              // marginBottom: "2.2%",
+              width: "60%",
+              alignItems: "center",
+              //backgroundColor: "red",
+              margin: "auto auto 50px auto",
+            }}
+          >
+            <h2>
+              With Bachelor Science of Chemisty, Jeffrey pocess strong research
+              mindset and analytical skillsets. He also enriched his computer
+              skills by taking school courses, internship and self-learning.
+              During his master study, Jeffrey put mosts effort in financial
+              data science and algorithms-related fields, such as algo trading
+              and quant research.
+            </h2>
+          </div> */}
           <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon hover:color-violet-600">
-              {skills}
-            </ul>
+            {/* <div
+              style={{
+                position: "absolute",
+                left: "100px",
+                top: "50px",
+                width: "auto",
+                height: "auto",
+                display: "block",
+              }}
+            >
+              <h1
+                style={{
+                  font: "Open Sans",
+                  fontWeight: "800",
+                  textAlign: "center",
+                  letterSpacing: "",
+                  color: "white",
+                }}
+              >
+                PROFICIENCY
+              </h1>
+            </div> */}
+            <ul className="list-inline mx-auto skill-icon ">{skills}</ul>
+          </div>
+          <div
+            style={{
+              //justifySelf: "center",
+              width: "60%",
+              height: "auto",
+              display: "block",
+              // backgroundColor: "red",
+              marginTop: "2.2%",
+              margin: "auto",
+            }}
+          >
+            <h3
+              style={{
+                font: "Open Sans",
+                fontWeight: "200",
+                textAlign: "center",
+                letterSpacing: "",
+                color: "white",
+              }}
+            >
+              {/* Others: Linux, Shell Script, Smart Contract, Solidiy, React, App
+              Development */}
+              With Bachelor Science of Chemisty, Jeffrey pocess strong research
+              mindset and analytical skillsets. He also enriched his computer
+              skills by taking school courses, internship and self-learning.
+              During his master study, Jeffrey put mosts effort in financial
+              data science and algorithms-related fields, such as algo trading
+              and quant research.
+            </h3>
           </div>
         </div>
-        <div id="pbar"></div>
       </section>
     );
   }
